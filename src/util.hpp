c023f14f815ced0330bb73dda0a015739f83b153
PH_modest <2692602032@qq.com>
2025-02-07 23:08:33 +0800

    文件实用工具类实现——读写操作

--- src/util.hpp
+++ src/util.hpp
@@ -61,5 +61,62 @@ namespace cloud
                 return _filename.substr(pos+1);
             }
         }
+        //获取文件指定位置，指定长度的数据
+        bool GetPosLen(std::string *body,size_t pos,size_t len)
+        {
+            //首先判断读取的长度是否会超出文件大小
+            size_t fsize = this->FileSize();
+            if(pos+len > fsize)
+            {
+                std::cout<<"get file len is error\n";
+                return false;
+            }
+            //获取文件数据
+            std::ifstream ifs;
+            ifs.open(_filename,std::ios::binary);//以二进制形式打开文件
+            if(ifs.is_open() == false)//判断是否打开成功
+            {
+                std::cout<<"read open file failed!\n";
+                return false;
+            }
+            ifs.seekg(pos,std::ios::beg);//从起始位置偏移pos单位
+            body->resize(len);//设置body大小
+            ifs.read(&(*body)[0],len);//读取len个长度的字符到body中
+            if(ifs.good() == false)//检查文件是否有问题
+            {
+                std::cout<<"get file content failed\n";
+                ifs.close();//关闭文件
+                return false;
+            }
+            ifs.close();
+            return true;
+        }
+        //获取文件数据
+        bool GetContent(std::string *body)
+        {
+            size_t fsize = this->FileSize();
+            return GetPosLen(body,0,fsize);
+        }
+        //向文件写入数据
+        bool SetContent(const std::string &body)
+        {
+            std::ofstream ofs;
+            ofs.open(_filename,std::ios::binary);
+            if(ofs.is_open()==false)
+            {
+                std::cout<<"write open file failed\n";
+                //ofs.close();
+                return false;
+            }
+            ofs.write(&body[0],body.size());
+            if(ofs.good()==false)
+            {
+                std::cout<<"write file content failed!\n";
+                ofs.close();
+                return false;
+            }
+            ofs.close();
+            return true;
+        }
     };
 }
